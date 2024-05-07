e0102b21ce3a0c22d12e20c1d2be38445a498dff
tjellicoe <thomas.jellicoe@telespazio.com>
2024-05-07 09:19:39 +0100

    Added app.py as provided by CEDA

--- stac_fastapi/elasticsearch/stac_fastapi/elasticsearch/app.py
+++ stac_fastapi/elasticsearch/stac_fastapi/elasticsearch/app.py
@@ -1,6 +1,16 @@
 """FastAPI application."""
 
 import os
+from typing import Any, Dict, Optional, cast
+
+from fastapi import Depends
+from fastapi.exceptions import HTTPException
+from fastapi.openapi.models import OAuthFlows as OAuthFlowsModel
+from fastapi.security import OAuth2
+from fastapi.security.utils import get_authorization_scheme_param
+from starlette.requests import Request
+from starlette.status import HTTP_401_UNAUTHORIZED
+from typing_extensions import Annotated, Doc
 
 from stac_fastapi.api.app import StacApi
 from stac_fastapi.api.models import create_get_request_model, create_post_request_model
@@ -29,6 +39,115 @@ from stac_fastapi.extensions.core import (
     CollectionSearchExtension,
 )
 from stac_fastapi.extensions.third_party import BulkTransactionExtension
+
+
+class OAuth2ClientCredentials(OAuth2):
+    """
+    OAuth2 flow for authentication using a bearer token obtained with an OAuth2 code
+    flow. An instance of it would be used as a dependency.
+    """
+
+    def __init__(
+        self,
+        tokenUrl: Annotated[
+            str,
+            Doc(
+                """
+                The URL to obtain the OAuth2 token.
+                """
+            ),
+        ],
+        refreshUrl: Annotated[
+            Optional[str],
+            Doc(
+                """
+                The URL to refresh the token and obtain a new one.
+                """
+            ),
+        ] = None,
+        scheme_name: Annotated[
+            Optional[str],
+            Doc(
+                """
+                Security scheme name.
+
+                It will be included in the generated OpenAPI (e.g. visible at `/docs`).
+                """
+            ),
+        ] = None,
+        scopes: Annotated[
+            Optional[Dict[str, str]],
+            Doc(
+                """
+                The OAuth2 scopes that would be required by the *"path" operations* that
+                use this dependency.
+                """
+            ),
+        ] = None,
+        description: Annotated[
+            Optional[str],
+            Doc(
+                """
+                Security scheme description.
+
+                It will be included in the generated OpenAPI (e.g. visible at `/docs`).
+                """
+            ),
+        ] = None,
+        auto_error: Annotated[
+            bool,
+            Doc(
+                """
+                By default, if no HTTP Auhtorization header is provided, required for
+                OAuth2 authentication, it will automatically cancel the request and
+                send the client an error.
+
+                If `auto_error` is set to `False`, when the HTTP Authorization header
+                is not available, instead of erroring out, the dependency result will
+                be `None`.
+
+                This is useful when you want to have optional authentication.
+
+                It is also useful when you want to have authentication that can be
+                provided in one of multiple optional ways (for example, with OAuth2
+                or in a cookie).
+                """
+            ),
+        ] = True,
+    ):
+        if not scopes:
+            scopes = {}
+        flows = OAuthFlowsModel(
+            clientCredentials=cast(
+                Any,
+                {
+                    "tokenUrl": tokenUrl,
+                    "refreshUrl": refreshUrl,
+                    "scopes": scopes,
+                },
+            )
+        )
+        super().__init__(
+            flows=flows,
+            scheme_name=scheme_name,
+            description=description,
+            auto_error=auto_error,
+        )
+
+    async def __call__(self, request: Request) -> Optional[str]:
+        authorization = request.headers.get("Authorization")
+        scheme, param = get_authorization_scheme_param(authorization)
+        if not authorization or scheme.lower() != "bearer":
+            if self.auto_error:
+                raise HTTPException(
+                    status_code=HTTP_401_UNAUTHORIZED,
+                    detail="Not authenticated",
+                    headers={"WWW-Authenticate": "Bearer"},
+                )
+            else:
+                return None  # pragma: nocover
+        return param
+
 
 settings = ElasticsearchSettings()
 session = Session.create_from_settings(settings)
@@ -72,6 +191,18 @@ extensions = [
 
 post_request_model = create_post_request_model(extensions)
 
+oauth2_scheme = OAuth2ClientCredentials(tokenUrl=os.getenv('TOKEN_URL'))
+
+dependencies = [Depends(oauth2_scheme)]
+routes = [
+    {"path": "/collections", "method": "POST"},
+    {"path": "/collections", "method": "PUT"},
+    {"path": "/collections/{collectionId}", "method": "DELETE"},
+    {"path": "/collections/{collectionId}/items", "method": "POST"},
+    {"path": "/collections/{collectionId}/items/{itemId}", "method": "PUT"},
+    {"path": "/collections/{collectionId}/items/{itemId}", "method": "DELETE"},
+]
+
 api = StacApi(
     title=os.getenv("STAC_FASTAPI_TITLE", "stac-fastapi-elasticsearch"),
     description=os.getenv("STAC_FASTAPI_DESCRIPTION", "stac-fastapi-elasticsearch"),
@@ -83,6 +214,7 @@ api = StacApi(
     ),
     search_get_request_model=create_get_request_model(extensions),
     search_post_request_model=post_request_model,
+    route_dependencies=[(routes, dependencies)],
 )
 app = api.app
 app.root_path = os.getenv("STAC_FASTAPI_ROOT_PATH", "")
